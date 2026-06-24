e31527c7eeccd9fd736d62412de5b537cca254d6
Marcossousadev <nossogm@gmail.com>
2026-06-24 13:59:23 -0300

    feat: new metadata and detail text hero-section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -6,7 +6,7 @@ export function HeroSection() {
     return (
         <section className="flex flex-col items-center bg-primary-background w-full min-h-screen ">
             <div className="flex flex-col items-center justify-center mt-10">
-                <h1 className="text-3xl font-bold text-center max-w-xs">Aumente a fidelidade e a satisfação dos seus clientes.</h1>
+                <h1 className="text-3xl font-bold text-center max-w-xs">Aumente a <span className="text-secondary">fidelidade</span> e a satisfação dos seus clientes.</h1>
                 <p className="text-sm text-center mt-4 max-w-2xs">Nós ajudamos empresas a construir relacionamentos mais fortes
                     com seus clientes por meio de soluções de design inovadoras.
                 </p>
